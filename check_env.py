34f9e185f369c8c5e6f0b0ddb40df75a2c292ef6
untitled-unrevised <snacky497@gmail.com>
2025-08-17 16:12:08 +0700

    why did i trust ai LOL

--- check_env.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pathlib
-import importlib.util
-
-print("Running Python:", sys.executable)
-spec = importlib.util.find_spec("bot.main")
-print("bot.main from :", pathlib.Path(spec.origin).resolve())
